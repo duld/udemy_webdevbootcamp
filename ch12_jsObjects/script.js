45aedc78f095712eaabc0fe9f1e78d20b38f281f
Jeffrey McCrea <duldren@gmail.com>
2017-07-16 01:15:42 -0700

    quick quiz solution

--- ch12_jsObjects/script.js
+++ ch12_jsObjects/script.js
@@ -1,1 +1,29 @@
 console.log("script is loaded");
+// Objects Quiz - Section 12 Lecture 136
+
+var someObject = {
+    friends: [
+        {name : 'Malfoy'},
+        {name : 'Crabbie'},
+        {name : 'Goyle'}
+    ],
+    color : "baby blue",
+    isEvil : true
+};
+
+// write code to retrive 'malfoy' from some object
+// go one layer at a time!
+
+// console.log(someObject.friends[0].name);
+// ==========================================================
+
+// movieDB section 12 Lecture 137
+// create an array of movie objects. Each movie should have a title, rating, 
+// and hasWatched properties.
+
+// output example:
+/*
+    You have watched "some movie" - 5 stars
+    You have not seen "some other movie" - 3 stars
+    etc...
+*/
