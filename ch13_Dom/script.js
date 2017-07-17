761e067ebd95e20f464de6ebac710117103c8afa
Jeffrey McCrea <duldren@gmail.com>
2017-07-16 19:45:33 -0700

    Dom Selection methods quiz

--- ch13_Dom/script.js
+++ ch13_Dom/script.js
@@ -1,1 +1,22 @@
 console.log("script loaded!");
+
+// selectiong
+/*
+    document.querySelector('css lookup')
+    document.getElementById('id name')
+    document.getElementsByClassName('classname')
+    document.getElementsByTagName('tag')
+*/
+
+// come up with four ways to selct the first paragraph
+
+var selP1 = document.getElementById("first");
+var selP2 = document.querySelector("p");
+var selP3 = document.querySelector(".special");
+var selP4 = document.querySelectorAll("body #first");
+
+// instead of adding a bunch of style properties toggle a class on or off
+
+function toggleAlign(elem){
+    elem.classList.toggle('right-align');
+}
