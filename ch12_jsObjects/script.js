1f317e4e9d8fc7779f7614233c7ff5acd5e90eb4
Jeffrey McCrea <duldren@gmail.com>
2017-07-16 17:43:41 -0700

    movieDB lecture 137

--- ch12_jsObjects/script.js
+++ ch12_jsObjects/script.js
@@ -27,3 +27,21 @@ var someObject = {
     You have not seen "some other movie" - 3 stars
     etc...
 */
+
+var movies = [
+    {title : "SpiderMan: Homecoming", hasWatched : false, rating : 4.5},
+    {title : "Gladiator", hasWatched : true, rating : 4.5},
+    {title : "War for the Planet of the Apes", hasWatched : false, rating : 5},
+    {title : "Baby Driver", hasWatched : false, rating : 4},
+    {title : "Cars 3", hasWatched : false, rating : 3}
+];
+
+function listMovies(){
+    movies.forEach( (movie) => {
+        var watched = movie.hasWatched ? "have watched " : "have not seen ";
+        console.log("You " + watched + "\"" + movie.title + "\"" + " - " + movie.rating + " stars");
+    });
+}
+
+// listMovies();
+
