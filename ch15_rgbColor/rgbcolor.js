8ef39620612bbbbe6673c0bfaaabf50578a529d5
Jeffrey McCrea <duldren@gmail.com>
2017-07-22 15:23:09 -0700

    DOCS add function comments

--- ch15_rgbColor/rgbcolor.js
+++ ch15_rgbColor/rgbcolor.js
@@ -1,16 +1,9 @@
 console.log("script loaded!");
 
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 0, 255)",
-];
+var colors = generateRandomRGBs(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#color-display");
 var messageDisplay = document.querySelector("#message");
 
@@ -23,9 +16,11 @@ for (var i = 0; i < squares.length; i++) {
     // add click listners to squares
     squares[i].addEventListener('click', function(){
         var clickedColor = this.style.backgroundColor;
+        
         if (clickedColor === pickedColor) {
             messageDisplay.innerHTML = "Correct!";
             console.log("its a match!");
+            changeColors(pickedColor);
         } else {
             console.log("wrong!");
             this.style.backgroundColor = '#232323';
@@ -35,15 +30,21 @@ for (var i = 0; i < squares.length; i++) {
 }
 
 
-// generate random rgb color
+/** Generates a psudo-random value within RGB range */
 function randomColor(){
-    return Math.floor(Math.random() * 255);
+    return Math.floor(Math.random() * 256);
 }
 
+/** Creates an rgb string suitable for applying to a style property */
 function randomRGB(){
     return "rgb(" + randomColor() + ", " + randomColor() + ", " + randomColor() + ")";
 }
 
+/**
+ * Generates n number of random RGB values
+ * @param {number} numColors - the number of rgb colors to generate
+ * @return {array} an array of rgb string values
+ */
 function generateRandomRGBs(numColors){
     var colors = [];
 
@@ -51,4 +52,23 @@ function generateRandomRGBs(numColors){
         colors.push(randomRGB());
     }
     return colors;
+}
+
+/**
+ * Change all the colored squares to match 'color'
+ * @param {rgb} color - string representing an rgb value
+ */
+function changeColors(color){
+    for (var i=0; i < colors.length; i++){
+        squares[i].style.background = color;
+    }
+}
+
+/**
+ * Randomly pick a color within the 'colors' array to set as
+ * the solution to the game.
+ */
+function pickColor(){
+    var random = Math.floor((Math.random() * colors.length));
+    return colors[random];
 }
