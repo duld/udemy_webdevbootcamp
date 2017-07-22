07be3844370d8163638d3a8054e0622d7684fdc8
Jeffrey McCrea <duldren@gmail.com>
2017-07-22 15:39:33 -0700

    h1 will adopt pickedColor val upon success

--- ch15_rgbColor/rgbcolor.js
+++ ch15_rgbColor/rgbcolor.js
@@ -1,7 +1,7 @@
 console.log("script loaded!");
 
 var colors = generateRandomRGBs(6);
-
+var h1 = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#color-display");
@@ -17,12 +17,12 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function(){
         var clickedColor = this.style.backgroundColor;
         
+        // test if the correct color is selected
         if (clickedColor === pickedColor) {
             messageDisplay.innerHTML = "Correct!";
-            console.log("its a match!");
             changeColors(pickedColor);
+            h1.style.backgroundColor = pickedColor;
         } else {
-            console.log("wrong!");
             this.style.backgroundColor = '#232323';
             messageDisplay.innerHTML = "Try Again";
         }
