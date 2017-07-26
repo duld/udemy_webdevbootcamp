0a9e59ec0e8d24f31a43b4433b265dca776d748e
Jeffrey McCrea <duldren@gmail.com>
2017-07-25 19:46:13 -0700

    Text box input - update span and li click listeners

--- ch18_todo/assets/js/todo.js
+++ ch18_todo/assets/js/todo.js
@@ -1,12 +1,43 @@
 // check off specific todos by clicking
 console.log('script loaded');
 
+var todoTextBox = $('input[type="text"]');
+
 // toggle todo list item checked off/ uncheck
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(event){
     $(this).toggleClass('checked-off');
+    event.stopPropagation();
 });
 
 // Clear todo list entry
-$('span').on('click', function(){
-    $(this).parent().remove();
+$('ul').on('click', 'span', function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// text box input
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13){
+        $('ul').append("<li><span>X</span> " + todoTextBox.val() + "</li>");
+        todoTextBox.val("");
+    }
 })
+
+
+$('#add_btn').on('click', 'li', function(){
+    $('ul').append("<li><span>X</span> " + todoTextBox.val() + "</li>");
+    todoTextBox.val('');
+});
+
+function removeTodo(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+}
+
+function checkOffTodo(event){
+    console.log(this);
+}
