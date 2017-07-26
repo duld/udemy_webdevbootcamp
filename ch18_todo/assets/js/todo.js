a9b4fbc25eff645d908be4bfeb421275acd6fc55
Jeffrey McCrea <duldren@gmail.com>
2017-07-25 18:45:33 -0700

    Add clear todo item logic

--- ch18_todo/assets/js/todo.js
+++ ch18_todo/assets/js/todo.js
@@ -1,7 +1,12 @@
 // check off specific todos by clicking
 console.log('script loaded');
 
-
+// toggle todo list item checked off/ uncheck
 $('li').on('click', function(){
     $(this).toggleClass('checked-off');
 });
+
+// Clear todo list entry
+$('span').on('click', function(){
+    $(this).parent().remove();
+})
