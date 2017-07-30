11bf6a9fcd3878633014c0769509f6889cd15d12
Jeffrey McCrea <duldren@gmail.com>
2017-07-29 17:33:50 -0700

    Add todo input hide/show

--- ch18_todo/assets/js/todo.js
+++ ch18_todo/assets/js/todo.js
@@ -1,13 +1,15 @@
 // check off specific todos by clicking
-console.log('script loaded');
+// console.log('script loaded');
 
 var todoTextBox = $('input[type="text"]');
+
 
 // toggle todo list item checked off/ uncheck
 $('ul').on('click', 'li', function(event){
     $(this).toggleClass('checked-off');
     event.stopPropagation();
 });
+
 
 // Clear todo list entry
 $('ul').on('click', 'span', function(event){
@@ -17,27 +19,17 @@ $('ul').on('click', 'span', function(event){
     event.stopPropagation();
 });
 
+
 // text box input
 $("input[type='text']").keypress(function(event){
     if (event.which === 13){
-        $('ul').append("<li><span>X</span> " + todoTextBox.val() + "</li>");
+        $('ul').append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoTextBox.val() + "</li>");
         todoTextBox.val("");
     }
 })
 
 
-$('#add_btn').on('click', 'li', function(){
-    $('ul').append("<li><span>X</span> " + todoTextBox.val() + "</li>");
-    todoTextBox.val('');
+$('h1 i').on('click', function(){
+    // hide or show the input field
+    $('input').fadeToggle();
 });
-
-function removeTodo(event){
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove();
-    });
-    event.stopPropagation();
-}
-
-function checkOffTodo(event){
-    console.log(this);
-}
