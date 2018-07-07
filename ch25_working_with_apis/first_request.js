8e040051e040f3dc76ec69c2d316fb7c3d920e0f
Jeffrey McCrea <duldren@gmail.com>
2018-07-07 09:41:13 -0700

    Finish playing with reqest and axios

--- ch25_working_with_apis/first_request.js
+++ ch25_working_with_apis/first_request.js
@@ -1,17 +1,30 @@
 let request = require('request');
 let axios = require('axios');
 
-request('http://google.com', (err, res, body) => {
+
+let yahoo_endpoint = 'https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22clovis%2C%20ca%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+request(`${yahoo_endpoint}`, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     console.log('request call has finished!');
+    const bodyData = JSON.parse(body);
+    console.log(bodyData.query.results.channel.astronomy.sunset);
   }
 });
 
-axios.get('http://google.com')
-  .then(res => {
-    console.log('axios .then call');
-    // console.log(res.data);
-  })
-  .catch(err => {
-    console.log(err);
-  })
+
+
+// axios.get(`${yahoo_endpoint}`)
+//   .then(res => {
+//     console.log('axios .then call');
+//     console.log(typeof res.data)
+//     console.log(res.data.query.results.channel.astronomy.sunset);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+
+let movieAPI_generalSearch = `http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb`;
+let movieAPI_movieIDSearch = `http://www.omdbapi.com/?i=tt3896198&apikey=thewdb`;
