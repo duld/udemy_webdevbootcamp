a9ac7e71daca9a6a2c308f07e18141a343afedd8
Jeffrey McCrea <duldren@gmail.com>
2018-07-14 12:32:01 -0700

    Add description fields to campground schema

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,21 +17,23 @@ mongoose.connect('mongodb://localhost:27017/YelpCamp', {useNewUrlParser: true})
 // Schema
 let campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 let Campground = mongoose.model('Campground', campgroundSchema);
 
 // Temp Data
-// let campgrounds = [
-//   {name: "Salmon Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-//   {name: "Granite Hill", image: "https://t00.deviantart.net/QyuTy1K7Nqumfx7aagdbgl1x7A0=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/2fd2/th/pre/i/2013/245/3/f/rapids_and_waterfalls_by_lightningthefox7-d6ktxtu.jpg"},
-//   {name: "Bent Fork", image: "https://parks.state.wa.us/ImageRepository/Document?documentID=10012"},
-//   {name: "Slasher Hangout", image: "http://www.outdoorexposurephoto.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/4/8/4855_01_m.jpg"}
-// ];
+let campgrounds = [
+  {name: "Salmon Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350", description: "So named due to an excess of Salmon fishermen who frequented this location. There are no known salmon in or around this location."},
+  {name: "Granite Hill", image: "https://t00.deviantart.net/QyuTy1K7Nqumfx7aagdbgl1x7A0=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/2fd2/th/pre/i/2013/245/3/f/rapids_and_waterfalls_by_lightningthefox7-d6ktxtu.jpg", description: "Enjoy a firm night's rest. If you crave a rock between your shoulder blades and mosquitos in your nose, look no further!"},
+  {name: "Bent Fork", image: "https://parks.state.wa.us/ImageRepository/Document?documentID=10012", description: "Who bent all these forks!? Someone better pick this shit up!"},
+  {name: "Slasher Hangout", image: "http://www.outdoorexposurephoto.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/4/8/4855_01_m.jpg", description: "Just stay home"}
+];
 
-// // Campground.insertMany(campgrounds)
-// //   .catch(err => console.log('there was a problem saving to MongoDB'))
+// // Insert the seed data
+// Campground.insertMany(campgrounds)
+//   .catch(err => console.log('there was a problem saving to MongoDB'))
 
 // ROUTES //
 // /
@@ -57,7 +59,8 @@ app.post('/campgrounds', (req, res) => {
   // redirect to /campgrounds
   let name = req.body.name;
   let image = req.body.image;
-  Campground.create({name, image})
+  let description = req.body.description;
+  Campground.create({name, image, description})
     .then(campground => {
       res.redirect('/campgrounds');
     })
@@ -71,6 +74,21 @@ app.post('/campgrounds', (req, res) => {
 // /campgrounds/new
 app.get('/campgrounds/new', (req, res) => {
   res.render('new.ejs');
+});
+
+
+app.get('/campgrounds/:id', (req, res) => {
+  // find the campground with the id
+  Campground.findById(req.params.id, {_id: 0, name: 1, description: 1, image: 1})
+    .then( campDoc => {
+      res.render('show', {campground: campDoc})
+    })
+    .catch(err => {
+      console.log('couldnt find campground');
+      res.redirect('/campgrounds');
+    })
+  // res.send(`<h1>param: ${req.params.id}</h1>`);
+  // res.render("show", {id: req.params.id});
 });
 
 // app.post('campgrounds/', (req, res) => {
