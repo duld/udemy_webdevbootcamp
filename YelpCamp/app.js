a03cad352c38667b213741668dee2bd9d9ad56ab
Jeffrey McCrea <duldren@gmail.com>
2018-07-22 11:00:16 -0700

    Extract campground logic into seperate file

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// Models
+const {Campground} = require('./models/campground');
+
 // Setup Express
 let app = express();
 let PORT = 3000;
@@ -14,27 +17,7 @@ mongoose.connect('mongodb://localhost:27017/YelpCamp', {useNewUrlParser: true})
   .then(() => console.log('connected to MongoDB'))
   .catch(err => console.log('There was a problem trying to connect to MongoDB', err));
 
-// Schema
-let campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-let Campground = mongoose.model('Campground', campgroundSchema);
-
-// Temp Data
-let campgrounds = [
-  {name: "Salmon Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350", description: "So named due to an excess of Salmon fishermen who frequented this location. There are no known salmon in or around this location."},
-  {name: "Granite Hill", image: "https://t00.deviantart.net/QyuTy1K7Nqumfx7aagdbgl1x7A0=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/2fd2/th/pre/i/2013/245/3/f/rapids_and_waterfalls_by_lightningthefox7-d6ktxtu.jpg", description: "Enjoy a firm night's rest. If you crave a rock between your shoulder blades and mosquitos in your nose, look no further!"},
-  {name: "Bent Fork", image: "https://parks.state.wa.us/ImageRepository/Document?documentID=10012", description: "Who bent all these forks!? Someone better pick this shit up!"},
-  {name: "Slasher Hangout", image: "http://www.outdoorexposurephoto.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/4/8/4855_01_m.jpg", description: "Just stay home"}
-];
-
-// // Insert the seed data
-// Campground.insertMany(campgrounds)
-//   .catch(err => console.log('there was a problem saving to MongoDB'))
-
+  
 // ROUTES //
 // /
 app.get('/', (req, res) => {
