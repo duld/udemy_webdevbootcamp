fefb835bd60b69d99d5850bfec42a41d10f47667
Jeffrey McCrea <duldren@gmail.com>
2018-07-29 11:57:01 -0700

    Add register route handlers

--- authentication/auth-demo/app.js
+++ authentication/auth-demo/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const passport = require('passport');
-const passportLocal = require('passport-local');
+const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 // local libs
 const User = require('./models/user');
@@ -12,6 +12,8 @@ const PORT = 3000;
 // Setup express //
 let app = express();
 app.set('view engine', 'ejs');
+// setup express middleware - body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 // setup express middleware - passport
 app.use(require('express-session')({
   secret: "OtterKilledOurTurkeyNowItsPersonal",
@@ -22,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Setup passport //
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -42,17 +45,44 @@ app.get('/secret', (req, res) => {
   res.render('secret');
 });
 
-app.get('/login', (req, res) => {
-  res.send('login!');
-});
-
+// Register Routes //
+// Render register form
 app.get('/register', (req, res) => {
   res.render('register');
+});
+// Create a new User
+app.post('/register', (req, res) => {
+  
+  User.register( new User({username: req.body.username}), req.body.password )
+    .then(user => {
+      console.log('new user created', user.username);
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secret');
+      });
+    })
+    .catch(err => {
+      console.log('\nthere was a problem registering a new user!');
+      console.log(err);
+      res.render('register');
+    });
+});
+
+// Login Routes //
+// Render login form
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: "/secret",
+  failureRedirect: '/login'
+  }), (req, res) => {
+
 });
 
 app.get('/logout', (req, res) => {
   res.send('logout');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and runninng on Port ${PORT}`)
